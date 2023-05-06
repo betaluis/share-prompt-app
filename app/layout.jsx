43f3df6184ec7fcaee0e87b6932b17a5fdc99e0f
waldo <luis.betancourt.dev@gmail.com>
2023-05-06 14:35:54 -0500

    Added nav component to the layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import "@styles/global.css";
 
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
 export const metadata = {
     title: "Prompt Share",
     description: "Discover and share ai prompts",
@@ -12,7 +15,10 @@ export default function RootLayout({ children }) {
                 <div className="main">
                     <div className="gradient" />
                 </div>
-                <main className="app">{children}</main>
+                <main className="app">
+                    <Nav />
+                    {children}
+                </main>
             </body>
         </html>
     )
