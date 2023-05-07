f55705d5b1d8acdcf249186e1b55d95be6752f1a
waldo <luis.betancourt.dev@gmail.com>
2023-05-07 17:17:32 -0500

    Form component

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,54 @@
-const Form = () => {
-  return (
-    <div>Form</div>
-  )
+import Link from "next/link";
+
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+    return (
+        <section className="w-full max-w-full flex-start flex-col">
+            <h1 className="head_text text-left">
+                <span className="blue_gradient">{type} Post</span>
+            </h1>
+            <p className="desc text-left max-w-md">
+                {type} a post to Prompt Share and share it with your friends.
+            </p>
+            <form 
+                action="" 
+                onSubmit={handleSubmit}
+                className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+            >
+                <label>
+                    <span className="text-base font-satoshi font-semibold text-gray-700">Your AI Prompt</span>
+                    <textarea
+                        value={post.prompt}
+                        onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+                        placeholder="Write your prompt here..."
+                        required
+                        className="form_textarea"
+                    />
+                </label>
+                <label>
+                    <span className="text-base font-satoshi font-semibold text-gray-700">
+                        Tag{" "}
+                        <span>(#product, #webdevelopment, #idea)</span>
+                    </span>
+                    <input
+                        value={post.tag}
+                        onChange={(e) => setPost({ ...post, tag: e.target.value })}
+                        placeholder="#tag"
+                        required
+                        className="form_input"
+                    />
+                </label>
+                <div>
+                    <Link href="/" className="text-gray-500 text-sm">Cancel</Link>
+                    <button
+                        type="submit"
+                        disabled={submitting}
+                    >
+                        {submitting ? `${type}ing...` : {type}}
+                    </button>
+                </div>
+            </form>
+        </section>
+    )
 }
 
 export default Form
