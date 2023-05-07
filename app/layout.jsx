ff5c2146e747adf1813dc1c58abb2ebcd6abb82b
waldo <luis.betancourt.dev@gmail.com>
2023-05-06 20:37:25 -0500

    Wrapping app with the provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,15 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>
-                <div className="main">
-                    <div className="gradient" />
-                </div>
-                <main className="app">
-                    <Nav />
-                    {children}
-                </main>
-            </body>
+            <Provider>
+                <body>
+                    <div className="main">
+                        <div className="gradient" />
+                    </div>
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </body>
+            </Provider>
         </html>
     )
 }
