efbb57b8e1a475b05b33f98f906190a91c1d30ae
waldo <luis.betancourt.dev@gmail.com>
2023-05-06 21:43:19 -0500

    Sign in and create user

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,7 @@
-import { connectToDatabase } from "@utils/database";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
-console.log({
-    clientId: process.env.GOOGLE_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-})
+import User from "@models/User";
+import { connectToDatabase } from "@utils/database";
 
 const handler = NextAuth({
     providers: [
@@ -15,20 +11,26 @@ const handler = NextAuth({
         }),
     ],
     async session({ session }) {
-},
+
+    },
     async signIn({ profile }) {
         try {
             connectToDatabasea();
-            // Check if user exists in database
-            
-            // If user does not exist, create user
+            const userExists = await User.findOne({ email: profile.email });
+            if (!userExists) {
+                const user = await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(/\s/g, "").toLowerCase(),
+                    image: profile.image,
+                });
 
-            // If user exists, return true to sign in
+                return true;
+            }
         } catch (error) {
             console.log(error);
             return false;
         }
-    }
+    },
 });
 
 export { handler as GET, handler as POST };
